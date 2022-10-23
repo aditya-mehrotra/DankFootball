1750184fb94605ef3efbc6f6b723dcfbc47ca3f7
Ankit Khurana <ankitkhurana8255@gmail.com>
2022-10-23 15:05:12 +0530

    Initial setup and navBar addition

---
+++ front-end/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import { NavBar } from './Components/NavBar';
+
+
+function App() {
+  return (
+    <>
+      <CssBaseline/>
+      <NavBar/>
+      <Container>
+      </Container>
+    </>
+  );
+}
+
+export default App;
